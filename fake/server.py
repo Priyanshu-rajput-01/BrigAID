17c0fed55ffbda8e347d972c8adef07a2c416943
Rohan Nawathe <rohannawathe@hotmail.com>
2021-06-04 19:58:12 +0530

    Added documentation for endpoint

--- fake/server.py
+++ fake/server.py
@@ -16,21 +16,37 @@ app = Flask(__name__)
 
 @app.route("/predict", methods=["POST"])
 def predict_medicine():
+    """
+    Return prediction about medicine in image.
+
+    Request Format:
+    Multipart Form Request with image to run inference sent with key "image"
+
+    Response Format:
+    JSON Response
+    {
+        "authentic" : <bool>,
+        "data" : <some text about the medicine>
+    }
+    """
 
     # read image file string data
     filestr = request.files["image"].read()
-    # log.debug("Image String : %s", filestr)
-    # convert string data to numpy array
+
+    # convert image string data to numpy array
     npimg = numpy.fromstring(filestr, numpy.uint8)
+
     # convert numpy array to image
     img = cv2.imdecode(npimg, cv2.IMREAD_UNCHANGED)
 
     # Run inference tasks here
     # results = infer(img)
 
+    # Testing code for image parsing
     retval, img_buffer = cv2.imencode(".jpg", img)
     log.debug("Return Val : %s \n Output : %s", retval, img_buffer)
 
+    # Return same image as input
     response = make_response(bytes(img_buffer))
     response.headers["Content-Type"] = "image/jpeg"
 
